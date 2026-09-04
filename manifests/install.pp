7639310e52ba19d3a1f9883ee2a1dfc8eb22b282
dgoldsmith <dgoldsmith@sans.org>
2026-09-04 13:16:36 -0400

    ownership changes so install is happy on Rocky 9

--- manifests/install.pp
+++ manifests/install.pp
@@ -72,10 +72,14 @@ class redmine::install {
   }
 
   exec { 'bundle_redmine':
-    command => "${redmine::bundle} install --gemfile ${redmine::install_dir}/Gemfile --without ${without_gems}",
+    command => "${redmine::bundle} install --gemfile ${redmine::install_dir}/Gemfile --path ${redmine::install_dir}/vendor/bundle --without ${without_gems}",
     creates => "${redmine::install_dir}/Gemfile.lock",
     require => [$bundler_require, Package['make'], Package['gcc'], Package[$packages]],
     notify  => Exec['rails_migrations'],
+  }
+  -->
+  exec { 'redmine-apache-ownership':
+    command => "/bin/chown -R ${apache::params::user}:${apache::params::group} ${redmine::install_dir} && /bin/chmod -R g+w ${redmine::install_dir}",
   }
 
   create_resources('redmine::plugin', $redmine::plugins)
